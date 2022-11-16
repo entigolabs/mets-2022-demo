1d17fc69f63a20fe3a5dc98df27021470fda9fa2
martivo <martin.vool@entigo.com>
2022-11-16 11:26:51 +0200

    Initial

--- up.sh
+++ up.sh
@@ -6,9 +6,9 @@
 # ssh ubuntu@server.prod.kind.learn.entigo.io
 #Clone the repo and install dependencies
 # For dev
-git clone --branch dev https://github.com/entigolabs/mets-2022.git
+git clone --branch dev https://github.com/entigolabs/mets-2022-demo.git
 # For test and prod
-# git clone --branch main https://github.com/entigolabs/mets-2022.git
+# git clone --branch main https://github.com/entigolabs/mets-2022-demo.git
 
 #Instlal dependencies (helm, kubectl, kind...)
 cd mets-2022 && ./install-dependencies.sh
@@ -28,9 +28,9 @@ kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.pas
 
 #Create argocd app-of-apps
   #For dev
-  helm template argocd-yaml/app-of-apps/ --set runenv="dev" --set domain="dev.kind.learn.entigo.io" --set gitbranch="dev" --set gitrepo="https://github.com/entigolabs/mets-2022.git"  | kubectl apply -n argocd -f-
+  helm template argocd-yaml/app-of-apps/ --set runenv="dev" --set gitbranch="dev" --set gitrepo="https://github.com/entigolabs/mets-2022-demo.git"  | kubectl apply -n argocd -f-
   #For test
-  helm template argocd-yaml/app-of-apps/ --set runenv="test" --set domain="test.kind.learn.entigo.io" --set gitbranch="main" --set gitrepo="https://github.com/entigolabs/mets-2022.git"  | kubectl apply -n argocd -f-
+  helm template argocd-yaml/app-of-apps/ --set runenv="test" --set gitbranch="main" --set gitrepo="https://github.com/entigolabs/mets-2022-demo.git"  | kubectl apply -n argocd -f-
   #For prod
-  helm template argocd-yaml/app-of-apps/ --set runenv="prod" --set domain="prod.kind.learn.entigo.io" --set gitbranch="main" --set gitrepo="https://github.com/entigolabs/mets-2022.git"  | kubectl apply -n argocd -f-
+  helm template argocd-yaml/app-of-apps/ --set runenv="prod" --set gitbranch="main" --set gitrepo="https://github.com/entigolabs/mets-2022-demo.git"  | kubectl apply -n argocd -f-
 
